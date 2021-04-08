ac6abbf69f4a27fcd22efe79f080a2be8f2973bf
murial <57202584+Murial@users.noreply.github.com>
2021-04-08 18:48:03 +0700

    total harga and total nama done

--- pertemuan5/pertemuan5/LatihanMenu.xaml.cs
+++ pertemuan5/pertemuan5/LatihanMenu.xaml.cs
@@ -31,12 +31,24 @@ namespace pertemuan5
 
         private void print_pesan(object sender, RoutedEventArgs e)
         {
-            var nama_makanan = new[] { "Nasi", "Ikan Gurame Bakar", "Cumi Asam Manis", "Tumis Kangkung", "Tahu Goreng", "Ayam Goreng"};
+            var nama_makanan = new[] { "Nasi", "Ikan Gurame Bakar", "Cumi Asam Manis", "Tumis Kangkung", "Tahu Goreng", "Ayam Goreng" };
+            var nama_minuman = new[] { "Teh Manis", "Jus Mangga", "Jus Alpukat", "Lemon Tea", "Milk Shake"};
             var harga_makanan = new[] { 5000, 60000, 45000, 15000, 5000, 12000 };
+            var harga_minuman = new[] { 4000, 8000, 8000, 5000, 9000};
 
-            var total_harga = makanan.Items.Cast<CheckBox>().Where(x => x.IsChecked == true).Select(x => harga_makanan[makanan.Items.IndexOf(x)]).Sum();
+            var total_nama_makanan = makanan.Items.Cast<CheckBox>().Where(x => x.IsChecked == true).Select(x => nama_makanan[makanan.Items.IndexOf(x)]);
+            var total_nama_minuman = minuman.Items.Cast<CheckBox>().Where(x => x.IsChecked == true).Select(x => nama_minuman[minuman.Items.IndexOf(x)]);
 
-            MessageBox.Show(string.Join(", ", total_harga));
+            var total_nama =  total_nama_makanan.Concat(total_nama_minuman);
+
+            var total_harga_makanan = makanan.Items.Cast<CheckBox>().Where(x => x.IsChecked == true).Select(x => harga_makanan[makanan.Items.IndexOf(x)]).Sum();
+            var total_harga_minuman = minuman.Items.Cast<CheckBox>().Where(x => x.IsChecked == true).Select(x => harga_minuman[minuman.Items.IndexOf(x)]).Sum();
+
+            var total_harga = total_harga_makanan + total_harga_minuman;
+
+            total.Text = "Rp." + string.Join(", ", total_harga) + ",00";
+
+            MessageBox.Show("Pesanan Anda : \n" + string.Join("\n ", total_nama));
         }
     }
 }
