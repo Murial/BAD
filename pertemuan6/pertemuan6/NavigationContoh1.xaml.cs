454612df9c93a5ab3bddf821bad1828b294f6712
murial <57202584+Murial@users.noreply.github.com>
2021-04-15 20:27:43 +0700

    pertemuan 6

---
+++ pertemuan6/pertemuan6/NavigationContoh1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace pertemuan6
+{
+    /// <summary>
+    /// Interaction logic for NavigationContoh1.xaml
+    /// </summary>
+    public partial class NavigationContoh1 : Window
+    {
+        public NavigationContoh1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnKlikP1(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+        }
+
+        private void BtnKlikP2(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page2();
+        }
+    }
+}
