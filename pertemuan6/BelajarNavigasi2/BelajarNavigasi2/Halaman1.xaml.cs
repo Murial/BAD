45e82c3fc752108e4bb0f632ca2e003f30669aa9
murial <57202584+Murial@users.noreply.github.com>
2021-04-15 20:31:18 +0700

    Add files via upload

---
+++ pertemuan6/BelajarNavigasi2/BelajarNavigasi2/Halaman1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BelajarNavigasi2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Halaman1 : Window
+    {
+        public Halaman1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPertama_Klik(object sender, RoutedEventArgs e)
+        {
+            Halaman2 hal2 = new Halaman2();
+            hal2.Show();
+            this.Close();
+        }
+    }
+}
